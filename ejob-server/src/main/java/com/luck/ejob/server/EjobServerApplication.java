a7a3760e610a12193528bf5e35df3ecf04833cd2
刘佳明 <liujiaming@192.168.0.100>
2019-12-16 21:34:09 +0800

    Initial commit

---
+++ ejob-server/src/main/java/com/luck/ejob/server/EjobServerApplication.java
@@ -0,0 +1,13 @@
+package com.luck.ejob.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjobServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjobServerApplication.class, args);
+    }
+
+}
